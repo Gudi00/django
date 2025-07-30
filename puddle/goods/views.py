24d67bc730d4ec00470fe583ddb72c4e8b5739b3
gudi <ioskomihailaa@gmail.com>
2025-07-30 17:02:33 +0300

    cool search

--- puddle/goods/views.py
+++ puddle/goods/views.py
@@ -3,23 +3,28 @@ from django.core.paginator import Paginator
 
 from goods.models import Products
 
+from goods.utils import q_search
+
 
 # Create your views here.
-def catalog(requset, category_slug):
+def catalog(requset, category_slug=None):
 
-    page = requset.GET.get('page', 1)
-    on_sale = requset.GET.get('on_sale', None)
-    order_by = requset.GET.get('order_by', None)
+    page = requset.GET.get("page", 1)
+    on_sale = requset.GET.get("on_sale", None)
+    order_by = requset.GET.get("order_by", None)
+    query = requset.GET.get("q", None)
 
-    if category_slug == 'all':
+    if category_slug == "all":
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
     if on_sale:
         goods = goods.filter(discount__gt=0)
 
-    if order_by and order_by != 'default':
+    if order_by and order_by != "default":
         goods = goods.order_by(order_by)
 
     paginator = Paginator(goods, 3)
@@ -28,7 +33,7 @@ def catalog(requset, category_slug):
     content = {
         "title": "Home - Каталог",
         "goods": current_page,
-        'slug_url': category_slug,
+        "slug_url": category_slug,
     }
 
     return render(requset, "goods/catalog.html", content)
@@ -39,7 +44,7 @@ def product(requset, product_slug):
     product = Products.objects.get(slug=product_slug)
 
     context = {
-        'product': product,
+        "product": product,
     }
 
     return render(requset, "goods/product.html", context)
--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
     "debug_toolbar",
    
@@ -83,9 +84,13 @@ WSGI_APPLICATION = 'puddle.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "home",
+        "USER": "home",
+        "PASSWORD": "root",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
