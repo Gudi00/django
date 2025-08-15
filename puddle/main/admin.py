c31b353257079ace75e24ae4ab359483ad512fa6
gudi <ioskomihailaa@gmail.com>
2025-08-15 23:20:17 +0300

    dont work (admin function)

--- puddle/main/admin.py
+++ puddle/main/admin.py
@@ -1,4 +1,29 @@
-from django.contrib import admin
 
-# Register your models here.
-  
+# goods/admin.py
+from django.contrib import admin
+from goods.models import Products, Categories
+from django import forms
+
+class ProductAdminForm(forms.ModelForm):
+    class Meta:
+        model = Products
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        user = self.current_user if hasattr(self, 'current_user') else None
+        if user and user.groups.filter(name='ContentEditor').exists():
+            # Restrict ContentEditor to description and image fields
+            self.fields = {k: v for k, v in self.fields.items() if k in ['description', 'image']}
+
+class ProductAdmin(admin.ModelAdmin):
+    form = ProductAdminForm
+    list_display = ('name', 'price', 'discount', 'quantity', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.current_user = request.user  # Pass user to form
+        return form
+
--- puddle/notifications/admin.py
+++ puddle/notifications/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin
+# # notifications/admin.py
+# from django.contrib import admin
+# from django.db.models import Count
+# from goods.models import Products
+# from orders.models import Order, OrderItem
 
-# Register your models here.
+# @admin.register(Products)
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'price', 'discount', 'quantity', 'orders_count')
+
+#     def orders_count(self, obj):
+#         return OrderItem.objects.filter(product=obj).count()
+#     orders_count.short_description = 'Number of Orders'
+
+# @admin.register(Order)
+# class OrderAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'user', 'created_timestamp', 'status', 'total_price')
+
+#     def total_price(self, obj):
+#         return obj.orderitem_set.total_price()
+#     total_price.short_description = 'Total Price'
